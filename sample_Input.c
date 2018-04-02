ebec20c52f18fe0af6cef01be13eb7cdec5091de
ABDUL HASIB <abdulhasibuddin@gmail.com>
2018-04-02 22:20:34 +0600

    Update sample_Input.c

--- sample_Input.c
+++ sample_Input.c
@@ -5,10 +5,16 @@ int main()
     char str[100];
     int a;
 
+    //This is a single line comment
     gets(str);
     scanf("%d",&a);
     printf("Enter a=%d in the console.",a);
 
+    /*
+        This is
+        a Multi-line comment.
+    */
+    printf("Following is a for loop:");
     for(int i=0; ;i++)
     {
         if(str[i]=='\0') { break; }
